90679b967a73e7336d684b668ee143e2f2ca6147
Renan Anchieta <renangap18@gmail.com>
2023-03-23 14:00:57 -0300

    primeiro commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tarefas: [
+      { id:1, titulo: "Estudar Vue 3", concluido: false },
+      { id:2, titulo: "Estudar Vuetify", concluido: false },
+      { id:3, titulo: "Estudando Vue 2 com Vuetify 2", concluido: false }
+    ]
   },
   getters: {
   },
   mutations: {
+    adicionarTarefa(state, titulo){
+      if(titulo){
+        state.tarefas.push({
+          id:new Date().getTime(),
+          titulo,
+          concluido: false
+        })
+      }
+    },
+    removeTarefa(state, id){
+      state.tarefas = state.tarefas.filter(tarefa => tarefa.id !== id)
+    }
   },
   actions: {
   },
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-col cols="12" sm="6">
+      <v-text-field v-model="input" label="Nova Tarefa" outlined clearable @keyup.enter="addTarefa" ></v-text-field>
+    </v-col>
+    <v-list flat subheader>
+      <v-list-item-group  multiple active-class="">
+        <div v-for="tarefa, index in $store.state.tarefas" :key="index">
+          <Tarefa :tarefa="tarefa" />
+        </div>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import Tarefa from '../components/tarefas/Tarefa.vue'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      input: null,
+    }
+  },
+  methods:{
+    addTarefa(){
+      this.$store.commit('adicionarTarefa', this.input);
+      this.input = null;
+    }
+  },
+
+  components: {
+    Tarefa
+  },
+
+}
+</script>
